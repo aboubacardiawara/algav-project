e14277d6a318e5ade583c5ed4cab3084bd0bb7f6
diawara <aboubacar.diawara.etu@univ-lille.fr>
2022-11-16 14:49:36 +0100

    better specification

--- src/test/warming_up.test.cpp
+++ src/test/warming_up.test.cpp
@@ -15,13 +15,13 @@ void testCompletion()
 {
 	// tronc
 	vector<bool> v1 = {false, true, true, false, false, true};
-	vector<bool> expected1 = {false, true, true, false, false, true};
-	assert(completion(v1, 4) == expected1);
+	vector<bool> expected1 = {false, true, true, false};
+	assert(completion(v1, 4) == expected1 && "should tronc");
 
 	// expand
 	vector<bool> v2 = {false, true, true, false, false, true};
 	vector<bool> expected2 = {false, true, true, false, false, true, false, false};
-	assert(completion(v2, 8) == expected2);
+	assert(completion(v2, 8) == expected2 && "should complete with 'false'");
 }
 
 int main() {
