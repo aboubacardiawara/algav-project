7015620b800feea445fa33851ed742e7d6a2fea5
diawara <aboubacar.diawara.etu@univ-lille.fr>
2022-12-07 11:37:02 +0100

    testing luka word

--- src/tests/tree.test.cpp
+++ src/tests/tree.test.cpp
@@ -4,8 +4,11 @@
 void test_tree_build()
 {
     BinaryDecisionTree test(38, 8);
-    test.BasicCompression();
+    //test.BasicCompression();
     cout << test.getLukasWord() << endl;
+    string expected_luka_word = "(((false)(true))((true)(false)))(((false)(true))((false)(false)))"; 
+    cout << expected_luka_word << endl;
+    assert(test.getLukasWord() == expected_luka_word && "test luka word");
 }
 
 int main(int argc, const char **argv)
